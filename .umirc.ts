2c1484fc5308cb6bb6e926f3cf7bb8107606a7b9
qinhui3 <qinhui3@shein.com>
2021-07-09 17:33:42 +0800

    feat: modify something

--- .umirc.ts
+++ .umirc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   outputPath: 'docs-dist',
   mode: 'site',
   base: '/ts-algorithms/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/ts-algorithms/' : '/',
+  publicPath: '/ts-algorithms/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   hash: true,
   navs: [
